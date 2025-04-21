f0fa1056a82d3f7b4ab06ff91d8188ebf719f3c0
Patrick Kennel <patrick@pksoftwareco.com>
2025-04-21 09:50:09 -0700

    fix typo

--- astrolabe/__main__.py
+++ astrolabe/__main__.py
@@ -1,4 +1,3 @@
-# Copyright # Copyright 2020 Life360, Inc
 # SPDX-License-Identifier: Apache-2.0
 
 from astrolabe.main import main
--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright # Copyright 2020 Life360, Inc
+# Copyright # Copyright 2025 LightWire, LLC
 # SPDX-License-Identifier: Apache-2.0
 
 from astrolabe.main import main
